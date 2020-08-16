13bc3f02e457d5dfe6ea8ea6e4b3acecf1d8db7a
khj68 <maga40@naver.com>
2020-08-16 18:54:16 +0900

    Add message DTO

---
+++ chat/src/main/java/jun/chat/data/ChatMessage.java
@@ -0,0 +1,14 @@
+package jun.chat.data;
+
+import lombok.Data;
+
+@Data
+public class ChatMessage {
+    public enum MessageType {
+        ENTER, TALK
+    }
+    private MessageType type;
+    private String roomId;
+    private String sender;
+    private String message;
+}
