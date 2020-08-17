4b563c38774a7fde45e4ffc5af174a27df4e09ee
khj68 <maga40@naver.com>
2020-08-17 15:02:15 +0900

    Simplify chatroom DTO

--- chat/src/main/java/jun/chat/data/ChatRoom.java
+++ chat/src/main/java/jun/chat/data/ChatRoom.java
@@ -2,6 +2,7 @@ package jun.chat.data;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 import org.springframework.web.socket.WebSocketSession;
 
@@ -13,23 +14,32 @@ import jun.chat.ChatService;
 public class ChatRoom {
     private String roomId;
     private String name;
-    private Set<WebSocketSession> sessions = new HashSet<>();
 
-    @Builder
-    public ChatRoom(String roomId, String name) {
-        this.roomId = roomId;
-        this.name = name;
+    public static ChatRoom create(String name) {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.roomId = UUID.randomUUID().toString();
+        chatRoom.name = name;
+        return chatRoom;
     }
 
-    public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService) {
-        if(chatMessage.getType().equals(ChatMessage.MessageType.ENTER)) {
-            sessions.add(session);
-            chatMessage.setMessage(chatMessage.getSender() + "님이 입장했습니다.");
-        }
-        sendMessage(chatMessage, chatService);
-    }
+    // websocket 사용 시 
+    // private Set<WebSocketSession> sessions = new HashSet<>();
 
-    public <T> void sendMessage(T message, ChatService chatService) {
-        sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
-    }
+    // @Builder
+    // public ChatRoom(String roomId, String name) {
+    //     this.roomId = roomId;
+    //     this.name = name;
+    // }
+
+    // public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService) {
+    //     if(chatMessage.getType().equals(ChatMessage.MessageType.ENTER)) {
+    //         sessions.add(session);
+    //         chatMessage.setMessage(chatMessage.getSender() + "님이 입장했습니다.");
+    //     }
+    //     sendMessage(chatMessage, chatService);
+    // }
+
+    // public <T> void sendMessage(T message, ChatService chatService) {
+    //     sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
+    // }
 }
